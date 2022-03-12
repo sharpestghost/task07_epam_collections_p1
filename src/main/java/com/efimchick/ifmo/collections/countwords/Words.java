d66d0afe8cdfaa99ef144db8b90da27b6966861d
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2022-03-12 03:02:20 +0300

    raw variant

--- src/main/java/com/efimchick/ifmo/collections/countwords/Words.java
+++ src/main/java/com/efimchick/ifmo/collections/countwords/Words.java
@@ -1,12 +1,43 @@
 package com.efimchick.ifmo.collections.countwords;
 
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Words {
 
-    public String countWords(List<String> lines) {
+    private static final String WORD_DETERMINER = "[^\\p{L}]+";
+    private final int MINIMAL_WORD_LENGTH = 4;
+    private final int MINIMAL_WORD_FREQUENCY = 10;
 
-        return null;
+    public String countWords(List<String> lines) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String s: lines) {
+            String[] words = s.toLowerCase(Locale.ROOT).split(WORD_DETERMINER);
+            for (String word: words) {
+                if (word.length() >= MINIMAL_WORD_LENGTH) {
+                    if (!map.containsKey(word)) {
+                        map.put(word, 1);
+                    } else {
+                        int count = map.get(word);
+                        map.put(word, count + 1);
+                }
+            }
+            }
+        }
+        return mapToString(map);
+
+    }
+
+    private String mapToString(Map<String, Integer> map) {
+        LinkedHashMap<String, Integer> reverseSortedMap = new LinkedHashMap<>();
+        map.entrySet()
+                .stream().sorted(Map.Entry.comparingByKey())
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
+        return reverseSortedMap.entrySet().stream().filter(x -> x.getValue() >= MINIMAL_WORD_FREQUENCY)
+                .map(key -> key.getKey().toLowerCase(Locale.ROOT) + " - " + key.getValue())
+                .collect(Collectors.joining("\n"));
+
     }
 }
